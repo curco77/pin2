75bedef2ddc8634ad638f023aa2e96a81708067e
curco77 <chelomena0902@gmail.com>
2025-08-07 01:14:43 -0400

    sr

--- api/log.js
+++ api/log.js
@@ -1,8 +1,12 @@
 export default async (req, res) => {
+  // Habilitar CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  
   if (req.method === 'POST') {
     const { lat, lon } = req.body;
     console.log('📍 Coordenadas:', lat, lon);
     return res.status(200).json({ success: true });
   }
-  return res.status(404).send('Ruta no encontrada'); // Cambiado a 404 para debug
+  return res.status(404).send('Ruta no encontrada');
 };
